5538d2296719a1d6654fc44aa435fee11e3e4dd0
Li Zexuan <zexuan.li1@student.unsw.edu.au>
2024-04-16 13:33:10 +1000

    update in 16 April, 2024,version 3.

--- toolkit/project2/zid_project2_etl.py
+++ toolkit/project2/zid_project2_etl.py
@@ -580,7 +580,6 @@ def _test_aj_ret_dict(tickers, start, end):
     """
 
     dict_ret = aj_ret_dict(tickers, start, end)
-
     msg = "This means `dict_ret = aj_ret_dict(tickers, start, end)`, print out dict_ret:"
     util.test_print(dict_ret, msg)
     return dict_ret
